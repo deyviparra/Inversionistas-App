0a4a6c498e49d0675f732c479fe21bc113da5d2f
Deyvi Parra <deyvi.pr@gmail.com>
2020-03-31 08:53:39 -0500

    AWS limpio

--- src/upload.js
+++ src/upload.js
@@ -14,18 +14,6 @@ const s3 = new AWS.S3({
     accessKeyId: ID,
     secretAccessKey: SECRET
 });
-// const params = {
-//     Bucket: BUCKET_NAME,
-//     CreateBucketConfiguration: {
-//         // Set your region here
-//         LocationConstraint: "sa-east-1"
-//     }
-// };
-
-// s3.createBucket(params, function(err, data) {
-//     if (err) console.log(err, err.stack);
-//     else console.log('Bucket Created Successfully', data.Location);
-// });
 
 function uploadFile(fileName,key) {
     // Read content from the file
