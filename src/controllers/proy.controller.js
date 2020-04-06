3be9411fe42d97b1be47ea39365d40c6af9e02dc
Andrés Calle <andrescalleuribe@gmail.com>
2020-04-06 18:22:26 -0500

    Versión etapa 2 (deploy done)

--- src/controllers/proy.controller.js
+++ src/controllers/proy.controller.js
@@ -2,20 +2,20 @@ const proyCtrl = {}
 const Proyecto = require("../models/Proyecto");
 const { unlink } = require("fs-extra");
 const path = require("path");
-const {uploadFile}=require('../upload.js')
+const { uploadFile } = require('../upload.js')
 
 var tipos = [];
 
-proyCtrl.renderProyForm =(req,res)=>{
-    res.render('proyectos/nuevo-p')
-    // const tipo = req.query
-    
-    // tipos.push(tipo)
-    // console.log (tipos)
+proyCtrl.renderProyForm = (req, res) => {
+  res.render('proyectos/nuevo-p')
+  // const tipo = req.query
+
+  // tipos.push(tipo)
+  // console.log (tipos)
 
 }
-proyCtrl.createNewProy =async (req,res)=>{
-  const { nombre, tipo, direccion, rango, municipio, departamento, estrato} = req.body;
+proyCtrl.createNewProy = async (req, res) => {
+  const { nombre, tipo, direccion, rango, municipio, departamento, estrato } = req.body;
   const newProyecto = new Proyecto({
     nombre,
     tipo,
@@ -28,53 +28,55 @@ proyCtrl.createNewProy =async (req,res)=>{
   if (typeof req.file === 'undefined') {
     newProyecto.imagePath = "/uploads/sinfotop.png";
   } else {
-    newProyecto.imagePath  = "https://inversionistas-bucket.s3-sa-east-1.amazonaws.com/" + req.file.filename;
-    await uploadFile(path.join(__dirname, '../public/uploads/' + req.file.filename),req.file.filename)
+    newProyecto.imagePath = "https://inversionistas-bucket.s3-sa-east-1.amazonaws.com/" + req.file.filename;
+    await uploadFile(path.join(__dirname, '../public/uploads/' + req.file.filename), req.file.filename)
 
   }
   newProyecto.galeria = [""];
   newProyecto.tipos = [""];
   await newProyecto.save();
-    req.flash('success_msg', 'Proyecto creado')
-    res.redirect('/menuppal')
+  req.flash('success_msg', 'Proyecto creado')
+  res.redirect('/menuppal')
 }
-proyCtrl.renderProy= async (req,res)=>{
-    const proyectos= await Proyecto.find()
-    res.render('proyectos/lista-p', {proyectos})
+proyCtrl.renderProy = async (req, res) => {
+  const proyectos = await Proyecto.find()
+  res.render('proyectos/lista-p', { proyectos })
 }
-proyCtrl.renderEditFormProy= async (req,res)=>{ 
+proyCtrl.renderEditFormProy = async (req, res) => {
   const proy = await Proyecto.findById(req.params.id)
-  res.render('proyectos/edit-proy',{proy})
+  res.render('proyectos/edit-proy', { proy })
 }
-proyCtrl.updateProy= async(req,res)=>{
-  const {nombre,tipo,direccion,rango,municipio,departamento,estrato}=req.body;
+proyCtrl.updateProy = async (req, res) => {
+  const { nombre, tipo, direccion, rango, municipio, departamento, estrato } = req.body;
   if (typeof req.file === 'undefined') {
-      await Proyecto.findByIdAndUpdate(req.params.id, {nombre,tipo,direccion,rango,municipio,departamento,estrato} )
-  }else{
-      const imagePath  = "/uploads/" + req.file.filename;
-      const proy = await Proyecto.findById(req.params.id)
-      unlink(path.resolve(path.join(__dirname, '../public'+ proy.imagePath)))
-      await Proyecto.findByIdAndUpdate(req.params.id, {nombre,tipo,direccion,rango,municipio,departamento,estrato,imagePath} )
-  }
-      req.flash('success_msg', 'Proyecto actualizado')
-      res.redirect('/proy')
-}
-proyCtrl.deleteProy= async (req,res)=>{
+    await Proyecto.findByIdAndUpdate(req.params.id, { nombre, tipo, direccion, rango, municipio, departamento, estrato })
+  } else {
+    const imagePath = "/uploads/" + req.file.filename;
     const proy = await Proyecto.findById(req.params.id)
-    await Proyecto.findByIdAndDelete(req.params.id)
-    unlink(path.resolve(path.join(__dirname, '../public'+ proy.imagePath)))
-    req.flash('error_msg', 'Proyecto eliminado')
-    res.redirect('/proy')
+    unlink(path.resolve(path.join(__dirname, '../public' + proy.imagePath)))
+    await Proyecto.findByIdAndUpdate(req.params.id, { nombre, tipo, direccion, rango, municipio, departamento, estrato, imagePath })
+  }
+  req.flash('success_msg', 'Proyecto actualizado')
+  res.redirect('/proy')
 }
-proyCtrl.renderFichaP = async (req,res)=>{
+proyCtrl.deleteProy = async (req, res) => {
+  const proy = await Proyecto.findById(req.params.id)
+  await Proyecto.findByIdAndDelete(req.params.id)
+  if (proy.imagePath !== '/uploads/sinfotop.png') {
+    unlink(path.resolve(path.join(__dirname, '../public' + proy.imagePath)))
+  }
+  req.flash('error_msg', 'Proyecto eliminado')
+  res.redirect('/proy')
+}
+proyCtrl.renderFichaP = async (req, res) => {
   const proyecto = await Proyecto.findById(req.params.id)
-    res.render('proyectos/ficha-p',{proyecto})
-}  
+  res.render('proyectos/ficha-p', { proyecto })
+}
 
 proyCtrl.renderTipo = async (req, res) => {
   const proyecto = await Proyecto.findById(req.query.id)
-  res.render('proyectos/tipos',{proyecto})
- 
+  res.render('proyectos/tipos', { proyecto })
+
 }
 proyCtrl.updateType = async (req, res) => {
   const { tipos } = await Proyecto.findById(req.params.id)
