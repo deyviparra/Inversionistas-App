76ba8ee9d769c6ac75c73e6be4cbba47af094bd5
Andrés Calle <andrescalleuribe@gmail.com>
2020-04-06 16:03:03 -0500

    IsAthenticated en user y registro

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -15,13 +15,13 @@ const {
 router.get('/usuarios/login', renderLogin)
 router.post('/usuarios/login', login)
 // Crear usuarios
-router.get('/usuarios/registro',  renderRegistro)
-router.post('/usuarios/registro',  registro)
+router.get('/usuarios/registro', isAuthenticated, renderRegistro)
+router.post('/usuarios/registro', isAuthenticated, registro)
 // Logout
-router.get('/usuarios/logout', logout)
+router.get('/usuarios/logout', isAuthenticated,logout)
 // Ficha usuario y actualizar
-router.get('/usuarios/perfil',  renderFichaU)
-router.get("/edit-user/:id",  renderEditFormUser);
-router.put("/user/edit/:id",  updateUser);
+router.get('/usuarios/perfil', isAuthenticated, renderFichaU)
+router.get("/edit-user/:id", isAuthenticated, renderEditFormUser);
+router.put("/user/edit/:id", isAuthenticated, updateUser);
 
 module.exports = router;
